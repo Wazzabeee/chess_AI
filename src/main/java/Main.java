40bc9ea1f18fb2634712edcb1fc2892b66dadc14
Clement Delteil <50945780+Wazzabeee@users.noreply.github.com>
2022-10-25 08:47:14 -0400

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,12 @@
+
+public class Main {
+    public static void main(String[] args) {
+        UCI.uciCommunication();
+        //uci
+        //isready
+        //position startpos moves e2e4
+        //go wtime 30000 btime 30000 winc 0 binc 0
+        //position startpos moves e2e4 a7a5 g1f3
+        //go
+    }
+}
---
+++ src/main/java/UCI.java
@@ -0,0 +1,84 @@
+import java.util.*;
+
+import chesslib.*;
+
+public class UCI {
+    static String ENGINENAME="IA_v0.1";
+    public static void uciCommunication() {
+        Scanner input = new Scanner(System.in);
+        Board board = new Board();
+        while (true)
+        {
+            String inputString=input.nextLine();
+            if ("uci".equals(inputString))
+            {
+                inputUCI();
+            }
+            else if (inputString.startsWith("setoption"))
+            {
+                inputSetOption(inputString);
+            }
+            else if ("isready".equals(inputString))
+            {
+                inputIsReady();
+            }
+            else if ("ucinewgame".equals(inputString))
+            {
+                inputUCINewGame();
+            }
+            else if (inputString.startsWith("position"))
+            {
+                //System.out.println("postion recognized");
+                inputPosition(inputString, board);
+            }
+            //else if ("go".equals(inputString))
+            else if (inputString.startsWith("go"))
+            {
+                inputGo(board);
+            }
+        }
+    }
+    public static void inputUCI() {
+        System.out.println("id name "+ENGINENAME);
+        System.out.println("id author Jonathan");
+        //options go here
+        System.out.println("uciok");
+    }
+    public static void inputSetOption(String inputString) {
+        //set options
+    }
+    public static void inputIsReady() {
+        System.out.println("readyok");
+    }
+    public static void inputUCINewGame() {
+        //add code here
+    }
+    public static void inputPosition(String input, Board board) {
+        //System.out.println(input);
+        input=input.substring(9).concat(" ");
+        if (input.contains("startpos ")) {
+            input=input.substring(9);
+        }
+        else if (input.contains("fen")) {
+            input=input.substring(4);
+            board.loadFromFen(input);
+        }
+        if (input.contains("moves")) {
+            //System.out.println(input);
+            input=input.substring(input.indexOf("moves")+6);
+            //System.out.println(input);
+            String[] moves = input.split("\\s+");
+            //System.out.println(moves[moves.length - 1]);
+
+            board.doMove(moves[moves.length - 1]);
+
+        }
+        }
+    public static void inputGo(@NotNull Board board) {
+        //search for first move
+        //System.out.println(board.getFen());
+        List<Move> moves = board.legalMoves();
+        System.out.println("Legal moves: " + moves);
+        System.out.println("bestmove " + moves.get(0));
+    }
+}
